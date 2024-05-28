e2db8a2c5948066bdfdd017c8d73891afb88091b
olaldeJP <olaldejp@gmail.com>
2024-05-28 12:02:51 -0300

    update

--- proyectEscalada/src/App.tsx
+++ proyectEscalada/src/App.tsx
@@ -3,15 +3,25 @@ import FondoFijo from "./components/FondoFijo/FondoFijo"
 import Conocenos from "./components/Conocenos/Conocenos";
 import Cards from "./components/Cards/Cards";
 import Form from "./components/Form/Formulario";
+import Accordion from "./components/ImageAccordion/ImageAccordion";
 import Footer from "./components/Footer/Footer";
+import img1 from "./components/ImageAccordion/1.jpg"
+import img2 from "./components/ImageAccordion/2.jpg"
+import img3 from "./components/ImageAccordion/3.jpg"
+import img4 from "./components/ImageAccordion/4.jpg"
+import img5 from "./components/ImageAccordion/5.jpg"
 function App() {
+
+  const images = [img1, img2, img3, img4, img5]
 
   return <div>
     <NavBar />
     <FondoFijo />
     <Conocenos />
+    <Accordion images={images} />
     <Cards />
     <Form />
+
     <Footer />
   </div>
 }
---
+++ proyectEscalada/src/components/ImageAccordion/ImageAccordion.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import './styles.css';
+
+interface Props {
+    images: string[];
+}
+
+
+const Accordion: React.FC<Props> = ({ images }) => {
+
+    const [activeIndex, setActiveIndex] = useState<number | null>(null);
+
+    const handleMouseEnter = (index: number) => {
+        setActiveIndex(index);
+    };
+
+    const handleMouseLeave = () => {
+        setActiveIndex(null);
+    };
+
+
+    return (
+        <div className="accordion">
+            {images.map((image, index) => (
+                <div
+                    key={index}
+                    className={`accordion-item ${activeIndex === index ? 'active' : ''}`}
+                    onMouseEnter={() => handleMouseEnter(index)}
+                    onMouseLeave={handleMouseLeave}
+                    style={{ backgroundImage: `url(${image})` }}
+                >
+
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default Accordion;
