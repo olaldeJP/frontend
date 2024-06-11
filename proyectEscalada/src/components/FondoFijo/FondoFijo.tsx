98ed24c102134b166daddd8e31ff3382693b0443
olaldeJP <olaldejp@gmail.com>
2024-06-11 18:24:27 -0300

    update

--- proyectEscalada/src/components/FondoFijo/FondoFijo.tsx
+++ proyectEscalada/src/components/FondoFijo/FondoFijo.tsx
@@ -1,20 +1,25 @@
 import fondoStyle from './FondoFijo.module.css'
 import { useState, useEffect } from 'react';
 function sectionBackImg() {
+
     const [loaded, setLoaded] = useState(false);
 
     useEffect(() => {
         const img = new Image();
-        img.src = 'ruta/a/tu/imagen-de-alta-calidad.jpg'; // Cambia esto por la ruta de tu imagen de alta calidad
+        img.src = '../../../public/images/F_3.jpg'; // Cambia esto por la ruta de tu imagen de alta calidad
         img.onload = () => setLoaded(true);
+        img.onerror = () => console.error('Error loading high-quality image');
     }, []);
 
     return (
-        <div className={fondoStyle.sectionImagen} style={{
-            backgroundImage: loaded
-                ? 'url(../../../public/images/F_3.jpg)'  // Cambia esto por la ruta de tu imagen de alta calidad
-                : 'url(../../../public/images/F_Cel.jpg)'  // Cambia esto por la ruta de tu imagen de baja calidad
-        }}>
+        <div
+            className={fondoStyle.sectionImagen}
+            style={{
+                backgroundImage: loaded
+                    ? 'url(../../../public/images/F_3.jpg)'  // Cambia esto por la ruta de tu imagen de alta calidad
+                    : 'url(../../../public/images/F_Cel.jpg)'  // Cambia esto por la ruta de tu imagen de baja calidad
+            }}
+        >
             {!loaded && (
                 <div className={fondoStyle.placeholder}>
                     <div className={fondoStyle.spinner}>Cargando...</div>
