f36dbacde9666494379f11b974dbccb7fb3cc14d
olaldeJP <olaldejp@gmail.com>
2024-06-12 19:13:15 -0300

    update

--- proyectEscalada/src/components/FondoFijo/FondoFijo.tsx
+++ proyectEscalada/src/components/FondoFijo/FondoFijo.tsx
@@ -1,22 +1,10 @@
 import fondoStyle from './FondoFijo.module.css'
-import { useState, useEffect } from 'react';
 
 function sectionBackImg() {
-    const [loaded, setLoaded] = useState(false);
-
-    useEffect(() => {
-        const img = new Image();
-        img.src = '/images/F_Cel.jpg';
-        img.onload = () => setLoaded(true);
-    }, []);
 
     return (
-        <div className={`${fondoStyle.sectionImagen} ${loaded ? fondoStyle.sectionImagen : fondoStyle.loading}`}>
-            {!loaded && (
-                <div className={fondoStyle.placeholder}>
-                    <div className={fondoStyle.spinner}></div>
-                </div>
-            )}
+        <div className={fondoStyle.sectionImagen}>
+
         </div>
     );
 }
